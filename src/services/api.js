6801159626e704eb8d0eaf16bf8b8e29c907fbd0
LuxuruMX <lixliu.max@gmail.com>
2025-12-11 10:42:42 -0600

    variables eliminadas de codigo

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Crear instancia de axios con configuración base
 const api = axios.create({
