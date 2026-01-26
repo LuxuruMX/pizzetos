43c88ea1f7e44573ba779c748c38ce5aceecd45d
LuxuruMX <lixliu.max@gmail.com>
2026-01-26 14:08:06 -0600

    paso a edicion

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -108,7 +108,7 @@ export const invalidateCache = () => {
 
 export const fetchDetalleVenta = async (idVenta) => {
   try {
-    const response = await api.get(`/pos/recrea-ticket/${idVenta}`);
+    const response = await api.get(`/pos/edit/${idVenta}/detalle`);
     return response.data;
   } catch (error) {
     console.error('Error al obtener detalle de venta:', error);
